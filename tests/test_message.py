42541ff20b17cfb24d58134679c20892171c911c
sondeko <sondeko143@gmail.com>
2023-03-27 23:09:28 +0900

    ログイン失敗時にトークンをリフレッシュするように

--- tests/test_message.py
+++ tests/test_message.py
@@ -1,15 +1,27 @@
 import unittest
 
+from yomiage.main import MessageType
 from yomiage.main import parse_message
 
 
 class TestMessage(unittest.TestCase):
-    def test_message_parse(self):
+    def test_chat_message_parse(self):
         message = ":foo!foo@foo.tmi.twitch.tv PRIVMSG #bar :bleedPurple"
-        chat_user, chat_channel, chat_message = parse_message(message)
-        self.assertEquals(chat_user, "foo")
-        self.assertEquals(chat_channel, "bar")
-        self.assertEquals(chat_message, "bleedPurple")
+        parsed = parse_message(message)
+        self.assertEquals(parsed.message_type, MessageType.CHAT_READ)
+        self.assertEquals(parsed.user, "foo")
+        self.assertEquals(parsed.channel, "bar")
+        self.assertEquals(parsed.chat_message, "bleedPurple")
+
+    def test_login_failed_message_parse(self):
+        message = ":tmi.twitch.tv NOTICE * :Login authentication failed\r\n"
+        parsed = parse_message(message)
+        self.assertEquals(parsed.message_type, MessageType.LOGIN_FAILED)
+
+    def test_unknown_message_parse(self):
+        message = "PING :tmi.twitch.tv\r\n"
+        parsed = parse_message(message)
+        self.assertEquals(parsed.message_type, MessageType.UNKNOWN)
 
 
 if __name__ == "__main__":
--- yomiage/main.py
+++ yomiage/main.py
@@ -2,61 +2,111 @@ import asyncio
 import logging
 import re
 from codecs import encode
+from dataclasses import dataclass
+from enum import Enum
 from socket import AF_INET
 from socket import SOCK_STREAM
 from socket import socket
 
+import click
 from pickledb import PickleDB
 from pickledb import load
 from websockets.client import connect
 from websockets.exceptions import ConnectionClosed
 
+from yomiage.auth import refresh_access_token
 from yomiage.config import Settings
 from yomiage.config import get_settings
 
 logger = logging.getLogger("websockets")
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.ERROR)
 logger.addHandler(logging.StreamHandler())
 
 
-PATTERN = re.compile(r":(.+)!.+@.+\.tmi\.twitch\.tv PRIVMSG #(.+) :(.+)")
+MESSAGE_PATTERN = re.compile(r":(.+)!.+@.+\.tmi\.twitch\.tv PRIVMSG #(.+) :(.+)")
+LOGIN_FAILED_PATTERN = re.compile(
+    r":tmi\.twitch\.tv NOTICE \* :Login authentication failed\s*"
+)
 
 logger = logging.getLogger("tasks")
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
+
+
+class MessageType(Enum):
+    CHAT_READ = 1
+    LOGIN_FAILED = 2
+    UNKNOWN = 999
+
+
+@dataclass
+class Message:
+    message_type: MessageType
+    user: str
+    channel: str
+    chat_message: str
 
 
 def parse_message(message: str):
-    match = re.findall(PATTERN, message)
-    if not match or len(match[0]) != 3:
-        logger.debug("does not match '%s", message)
-        return "", "", ""
-    return match[0]
+    match = re.findall(MESSAGE_PATTERN, message)
+    if match and len(match[0]) == 3:
+        return Message(
+            message_type=MessageType.CHAT_READ,
+            user=match[0][0],
+            channel=match[0][1],
+            chat_message=match[0][2],
+        )
+    match = re.findall(LOGIN_FAILED_PATTERN, message)
+    if match:
+        return Message(
+            message_type=MessageType.LOGIN_FAILED,
+            user="",
+            channel="",
+            chat_message="",
+        )
+    return Message(
+        message_type=MessageType.UNKNOWN,
+        user="",
+        channel="",
+        chat_message="",
+    )
 
 
 def send_message(chat_message: str, port: int):
     message = "t" + chat_message + "\n"
-    logger.info(message)
+    logger.debug(message)
     output_bytes = encode(message, "utf-8", errors="replace")
-    with socket(AF_INET, SOCK_STREAM) as sock:
-        sock.connect(("localhost", port))
-        sock.sendall(output_bytes)
+    try:
+        with socket(AF_INET, SOCK_STREAM) as sock:
+            sock.connect(("localhost", port))
+            sock.sendall(output_bytes)
+    except ConnectionRefusedError:
+        logger.warning("The speech process does not seem to working.")
 
 
-async def read_chat(
-    uri: str, access_token: str, username: str, channel: str, port: int
-):
+async def read_chat(uri: str, username: str, channel: str, port: int, db: PickleDB):
     async for connection in connect(uri):
         try:
-            await connection.send(f"PASS oauth:{access_token}")
+            await connection.send(f"PASS oauth:{db.get('access_token')}")
             await connection.send(f"NICK {username}")
             await connection.send(f"JOIN #{channel}")
             async for message in connection:
-                chat_user, chat_channel, chat_message = parse_message(str(message))
-                if not chat_message:
+                parsed = parse_message(str(message))
+                if parsed.message_type == MessageType.CHAT_READ:
+                    logger.info(
+                        "In %s, %s says %s",
+                        parsed.channel,
+                        parsed.user,
+                        parsed.chat_message,
+                    )
+                    send_message(parsed.chat_message, port)
+                elif parsed.message_type == MessageType.LOGIN_FAILED:
+                    settings = get_settings()
+                    refresh_access_token(db, settings)
+                    logger.info("Refresh access token.")
+                    break
+                elif parsed.message_type == MessageType.UNKNOWN:
                     continue
-                logger.debug("In %s, %s says %s", chat_channel, chat_user, chat_message)
-                send_message(chat_message, port)
         except ConnectionClosed:
             continue
 
@@ -65,15 +115,19 @@ def run_bot(db: PickleDB, settings: Settings):
     asyncio.run(
         read_chat(
             "wss://irc-ws.chat.twitch.tv:443",
-            str(db.get("access_token")),
             settings.username,
             settings.channel,
             settings.speech_port,
+            db,
         )
     )
 
 
-def main():
+@click.command()
+@click.option("-r", "--refresh", is_flag=True)
+def main(refresh: bool):
     settings = get_settings()
     db = load(settings.db_file, auto_dump=False)
+    if refresh:
+        refresh_access_token(db, settings)
     run_bot(db, settings)

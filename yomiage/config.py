fc6f446f2329428f33f0bbe5e5d8104df0cc32d5
sondeko <sondeko143@gmail.com>
2023-05-15 01:55:10 +0900

    chat 読み上げ部分

--- yomiage/config.py
+++ yomiage/config.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+from pathlib import Path
 from typing import Any
 
 from pydantic import BaseSettings
@@ -11,16 +12,22 @@ class Settings(BaseSettings):
     username: str
     speech_port: int
     operations: list[str]
-    db_file: str = "./pickle.db"
+    db_dir: Path = Path("db")
+    db_name: str = "data.json"
 
     class Config(BaseSettings.Config):
         env_file = ".env"
+        env_prefix = "cb_"
 
         @classmethod
         def parse_env_var(cls, field_name: str, raw_val: str) -> Any:
             if field_name == "operations":
                 return [x.strip() for x in raw_val.split(",")]
             return cls.json_loads(raw_val)
+
+    @property
+    def db_file(self) -> str:
+        return str(self.db_dir.joinpath(self.db_name))
 
 
 @lru_cache()

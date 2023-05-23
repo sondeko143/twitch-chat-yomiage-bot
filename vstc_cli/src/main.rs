39b1d4b51fd4374c93cc137c1ccf7be9a0c5ec1d
sondeko <sondeko143@gmail.com>
2023-05-23 20:03:18 +0900

    [cli] 音声ファイルインプット

--- vstc_cli/src/main.rs
+++ vstc_cli/src/main.rs
@@ -1,5 +1,6 @@
+mod sound;
 use clap::Parser;
-use std::path::PathBuf;
+use std::{fs::File, path::PathBuf};
 use vstc;
 use vstreamer_protos::Sound;
 
@@ -34,7 +35,19 @@ async fn main() {
     let host = args.host.unwrap_or(String::from("localhost"));
     let port = args.port.unwrap_or(8080);
 
-    let sound: Option<Sound> = Some(Sound::default());
+    let sound: Option<Sound> = match args.wav {
+        Some(wav_path) => {
+            let mut wav_file = File::open(wav_path).unwrap();
+            let (header, data) = sound::read(&mut wav_file).unwrap();
+            Some(Sound {
+                data,
+                rate: header.sampling_rate,
+                format: sound::convert_format(&header),
+                channels: header.channel_count.into(),
+            })
+        }
+        None => Some(Sound::default()),
+    };
 
     vstc::process_command(
         format!("http://{host}:{port}").as_str(),
---
+++ vstc_cli/src/sound.rs
@@ -0,0 +1,96 @@
+use std::io::{self, Read};
+use wav::Header;
+
+pub fn read<R>(reader: &mut R) -> io::Result<(Header, Vec<u8>)>
+where
+    R: Read + io::Seek,
+{
+    let header = read_header(reader)?;
+    Ok((header, read_data(reader)?))
+}
+
+fn read_header<R>(reader: &mut R) -> io::Result<Header>
+where
+    R: Read + io::Seek,
+{
+    let wav = verify_wav_file(reader)?;
+
+    for c in wav.iter(reader) {
+        if c.id().as_str() == "fmt " {
+            // Read header contents
+            let header_bytes = c.read_contents(reader)?;
+            let header = Header::try_from(header_bytes.as_slice())
+                .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+            // Return error if not using PCM
+            match header.audio_format {
+                wav::WAV_FORMAT_PCM | wav::WAV_FORMAT_IEEE_FLOAT => return Ok(header),
+                _ => {
+                    return Err(io::Error::new(
+                        io::ErrorKind::Other,
+                        "Unsupported data format, data is not in uncompressed PCM format, aborting",
+                    ))
+                }
+            };
+        }
+    }
+
+    Err(io::Error::new(
+        io::ErrorKind::InvalidData,
+        "RIFF data is missing the \"fmt \" chunk, aborting",
+    ))
+}
+
+pub fn convert_format(header: &Header) -> i32 {
+    match header.audio_format {
+        wav::WAV_FORMAT_PCM => match header.bits_per_sample {
+            8 => 2,
+            16 => 4,
+            24 => 8,
+            _ => 0,
+        },
+        wav::WAV_FORMAT_IEEE_FLOAT => match header.bits_per_sample {
+            32 => 16,
+            _ => 0,
+        },
+        _ => 0,
+    }
+}
+
+fn read_data<R>(reader: &mut R) -> io::Result<Vec<u8>>
+where
+    R: Read + io::Seek,
+{
+    let wav = verify_wav_file(reader)?;
+
+    for c in wav.iter(reader) {
+        if c.id().as_str() == "data" {
+            // Read data contents
+            let data_bytes = c.read_contents(reader)?;
+            return Ok(data_bytes);
+        }
+    }
+
+    Err(io::Error::new(
+        io::ErrorKind::Other,
+        "Could not parse audio data",
+    ))
+}
+
+fn verify_wav_file<R>(reader: &mut R) -> io::Result<riff::Chunk>
+where
+    R: Read + io::Seek,
+{
+    let wav = riff::Chunk::read(reader, 0)?;
+
+    let form_type = wav.read_type(reader)?;
+
+    if form_type.as_str() == "WAVE" {
+        Ok(wav)
+    } else {
+        Err(io::Error::new(
+            io::ErrorKind::Other,
+            "RIFF file type not \"WAVE\"",
+        ))
+    }
+}

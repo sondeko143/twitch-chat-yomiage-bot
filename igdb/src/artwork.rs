b777001086c71469c885f72bc4075b06b535450d
sondeko <sondeko143@gmail.com>
2026-06-24 06:28:46 +0900

    feat(igdb): artwork/IGDB を独立クレート igdb に切り出し

---
+++ igdb/src/artwork.rs
@@ -0,0 +1,65 @@
+use crate::api;
+use anyhow::bail;
+use image::io::Reader as ImageReader;
+use image::RgbaImage;
+use log::{info, warn};
+use std::io::Cursor;
+
+pub async fn get_artwork(
+    client_id: &str,
+    client_secret: &str,
+    names: &[String],
+) -> anyhow::Result<()> {
+    let access_token = api::get_tokens_by_client_credentials(client_id, client_secret).await?;
+    let mut games: Vec<i64> = vec![];
+    for name in names {
+        let game_id = match game_ids(name, &access_token, client_id).await {
+            Ok(r) => r,
+            Err(e) => {
+                warn!("{}", e);
+                continue;
+            }
+        };
+        games.push(game_id);
+    }
+    let covers = api::get_cover(&access_token, client_id, &games).await?;
+    let image_width: u32 = 1252;
+    let mut img = RgbaImage::new(image_width, 704);
+    for (idx, game) in games.iter().enumerate() {
+        let cover = covers
+            .iter()
+            .find(|x| x.game == *game)
+            .unwrap_or_else(|| panic!("game id {} not found", game));
+        info!("{}: {}", cover.game, cover.image_id);
+        let content = reqwest::Client::new()
+            .get(format!(
+                "https://images.igdb.com/igdb/image/upload/t_cover_big_2x/{}.jpg",
+                cover.image_id
+            ))
+            .send()
+            .await?
+            .bytes()
+            .await?;
+        let on_top = ImageReader::new(Cursor::new(content))
+            .with_guessed_format()?
+            .decode()?;
+        let len: u32 = covers.len().try_into().unwrap();
+        let index: u32 = idx.try_into().unwrap();
+        let offset_x: u32 = (image_width / len) * index;
+        image::imageops::overlay(&mut img, &on_top, offset_x.into(), 0);
+    }
+    img.save("thumbnails.jpg")?;
+    Ok(())
+}
+
+async fn game_ids(name: &str, access_token: &str, client_id: &str) -> anyhow::Result<i64> {
+    let games = api::search_game(access_token, client_id, name).await?;
+    if games.is_empty() {
+        bail!("Not found {}", name)
+    }
+    info!(
+        "{}: {}, cover: {}",
+        games[0].id, games[0].name, games[0].cover
+    );
+    Ok(games[0].id)
+}
---
+++ igdb/src/main.rs
@@ -0,0 +1,47 @@
+mod api;
+mod artwork;
+mod settings;
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+use settings::Settings;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(short, long)]
+    config: Option<PathBuf>,
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    GetArtwork { names: Vec<String> },
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenv::dotenv().ok();
+    let args = Cli::parse();
+    let mut config_builder = config::Config::builder();
+    config_builder = config_builder
+        .add_source(config::Environment::with_prefix("cb").try_parsing(true));
+    if let Some(path) = args.config {
+        config_builder =
+            config_builder.add_source(config::File::with_name(path.to_str().unwrap()));
+    }
+    let config = config_builder.build()?;
+    let settings: Settings = config.try_deserialize()?;
+    simple_logger::SimpleLogger::new()
+        .env()
+        .with_local_timestamps()
+        .init()?;
+
+    match &args.command {
+        Some(Commands::GetArtwork { names }) => {
+            artwork::get_artwork(&settings.client_id, &settings.client_secret, names).await?;
+        }
+        None => {}
+    }
+    Ok(())
+}
